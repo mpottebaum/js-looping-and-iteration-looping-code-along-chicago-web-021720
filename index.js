ec0dbddf52f0e2ec6d9ddb30769b2064f08f50ad
mpottebaum <mpottebaum@gmail.com>
2020-03-29 18:53:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(strings, event) {
+    let array = [];
+    for (let i = 0; i < strings.length; i++) {
+        console.log(strings[i]);
+        array.push(`Thank you, ${strings[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
